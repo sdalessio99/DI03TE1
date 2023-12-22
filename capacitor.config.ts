b2f98cdaf4afc9e816a8af3ab8037d5c1059f2c4
Sara D'Alessio <sdalessio@birt.eus>
2023-12-22 21:21:00 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'DI03TE1App',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
